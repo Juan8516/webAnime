67e9541a4472ae8df78e79a31e55834f153c82dd
Juan8516 <juan.ind85@gmail.com>
2023-09-02 07:24:08 -0500

    Se agrega file de assets e index

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+/*=============== MOSTRAR MENU ===============*/
+
+/*===== MOSTRAR MENU =====*/
+/* validar si existe constante */
+
+/*===== MENU HIDDEN =====*/
+/* validar si existe constante */
+
+/*=============== SCROLL HEADER ===============*/
+
+/*=============== BRAND SWIPER ===============*/
+var brandSwiper = new Swiper(".brand-swiper", {
+    spaceBetween: 24,
+    loop: true,
+    breakpoints: {
+        450: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+            },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 24,
+        },
+    },
+  });
+
+/*=============== ANIME SWIPER ===============*/
