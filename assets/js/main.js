36bbf380f69edaaef754680da1ce38320f1ca558
Juan8516 <juan.ind85@gmail.com>
2023-09-03 17:25:29 -0500

    seccion espaecial

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,26 @@ var brandSwiper = new Swiper(".brand-swiper", {
   });
 
 /*=============== ANIME SWIPER ===============*/
+var animeSwiper = new Swiper(".anime-swiper", {
+    spaceBetween: 24,
+    loop: true,
+    breakpoints: {
+        450: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+            },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 24,
+        },
+    },
+  });
+
