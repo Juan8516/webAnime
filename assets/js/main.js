dc8aac837f685dd4912392e46af7699137e992e3
Juan8516 <juan.ind85@gmail.com>
2023-09-20 15:25:09 -0500

    estilos header - correccion logo twitter

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,17 @@
 /* validar si existe constante */
 
 /*=============== SCROLL HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+
+    if (window.scrollY >= 40) {
+        header.classList.add('scroll-header')
+    }else {
+        header.classList.remove('scroll-header')
+    } 
+}
+
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== BRAND SWIPER ===============*/
 var brandSwiper = new Swiper(".brand-swiper", {
